966fabbd7c7b6b12db76e6b02bd5667e1e26181e
Isaeck29 <isaeck29@innlandetfylke.no>
2026-09-24 10:38:21 +0200

    lagt til at det tallet telleren er på nå vises uten at man må åpne konsollen.

--- Enkel-Teller/script.js
+++ Enkel-Teller/script.js
@@ -2,17 +2,20 @@ let teller = 0;
 
 function telle() {
 teller = teller + 1;
+document.getElementById("tallsomvises").innerHTML = teller;
 console.log(teller);
 }
 
 
 function telle2() {
 teller = teller + 2;
+document.getElementById("tallsomvises").innerHTML = teller;
 console.log(teller);
 }
 
 
 function telleminus() {
 teller = teller - 1;
+document.getElementById("tallsomvises").innerHTML = teller;
 console.log(teller);
 }
