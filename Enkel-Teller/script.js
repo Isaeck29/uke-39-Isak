296d8a3bfe3da0a306c621a8844763b8d80d9315
Isaeck29 <isaeck29@innlandetfylke.no>
2026-09-24 10:27:39 +0200

    lagt til flere knapper med funksjoner

--- Enkel-Teller/script.js
+++ Enkel-Teller/script.js
@@ -4,3 +4,15 @@ function telle() {
 teller = teller + 1;
 console.log(teller);
 }
+
+
+function telle2() {
+teller = teller + 2;
+console.log(teller);
+}
+
+
+function telleminus() {
+teller = teller - 1;
+console.log(teller);
+}
