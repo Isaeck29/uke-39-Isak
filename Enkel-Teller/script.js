d8e9a7b0270758accf1741f0dfcc733e149407f2
Isaeck29 <isaeck29@innlandetfylke.no>
2026-09-24 10:45:19 +0200

    lagt til enda flere knapper.

--- Enkel-Teller/script.js
+++ Enkel-Teller/script.js
@@ -20,3 +20,21 @@ document.getElementById("tallsomvises").innerHTML = teller;
 console.log(teller);
 }
 
+
+function telle10() {
+teller = teller + 10;
+document.getElementById("tallsomvises").innerHTML = teller;
+console.log(teller);
+}
+
+function telleminus2() {
+teller = teller - 2;
+document.getElementById("tallsomvises").innerHTML = teller;
+console.log(teller);
+}
+
+function telleminus10() {
+teller = teller - 10;
+document.getElementById("tallsomvises").innerHTML = teller;
+console.log(teller);
+}
