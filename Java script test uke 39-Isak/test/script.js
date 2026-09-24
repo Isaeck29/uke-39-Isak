bc97d106a42b78b4e7cc9d5f5a00058d4eadaef8
Isaeck29 <isaeck29@innlandetfylke.no>
2026-09-24 09:44:58 +0200

    lagt til variabler og laget et lite regnestykke.

--- Java script test uke 39-Isak/test/script.js
+++ Java script test uke 39-Isak/test/script.js
@@ -1,2 +1,10 @@
 console.log("Hallo verden!");
-console.log("Det er veldig kult at du har funnet denne teksten, bra jobba!")
+console.log("Det er veldig kult at du har funnet denne teksten, bra jobba!");
+console.log("Denne linja er skrevet av en kommando.");
+let x= 5;
+let y= 6;
+let sum = x + y;
+
+console.log(sum);
+
+document.getElementById("demo").innerHTML = "Summen av x og y er: " + sum;
